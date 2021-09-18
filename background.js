0cf54464237dcf77e49cc10a2f920c159cf83b48
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2021-09-17 20:09:48 -0700

    Test: Sending message from background to content

--- background.js
+++ background.js
@@ -1,5 +1,12 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            //Tabs which satisfy these conditions: 
 
+            var activeTabs = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {'message': 'clicked_browser_action'})
+
+
+        }); //Figure out which tab I'm on
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
